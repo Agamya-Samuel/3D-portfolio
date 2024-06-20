3b60b01d5c232fb5733663c3c13c97e7a6560ee1
Agamya Samuel <48693970+Agamya-Samuel@users.noreply.github.com>
2024-06-21 03:48:06 +0530

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -32,36 +32,36 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-        {
-          from_name: form.name,
-          to_name: "JavaScript Mastery",
-          from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
-          message: form.message,
-        },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setLoading(false);
+		.send(
+			import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+			import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+			{
+				from_name: form.name,
+				to_name: 'Agamya Samuel',
+				from_email: form.email,
+				to_email: import.meta.env.VITE_APP_EMAILJS_EMAIL_ID,
+				message: form.message,
+			},
+			import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+		)
+		.then(
+			() => {
+				setLoading(false);
           alert("Thank you. I will get back to you as soon as possible.");
 
-          setForm({
+				setForm({
             name: "",
             email: "",
             message: "",
-          });
-        },
-        (error) => {
-          setLoading(false);
-          console.error(error);
+				});
+			},
+			(error) => {
+				setLoading(false);
+				console.error(error);
 
           alert("Ahh, something went wrong. Please try again.");
-        }
-      );
+			}
+		);
   };
 
   return (
